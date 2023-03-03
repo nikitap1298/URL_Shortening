80c802e2a1e3cd1578254b33cbacff60b24f7a8b
Nikita <nikita.pishchugin@gmail.com>
2023-03-03 12:50:43 +0100

    Finish Nav Bar

--- index.js
+++ index.js
@@ -3,17 +3,8 @@ const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
-const topNavWidth = () => {
-  var x = document.getElementById("top-Nav")
-  if (x.className === "top-Nav") {
-    x.className += " responsive"
-  } else {
-    x.className = "top-Nav"
-  }
-}
-
-// Body Parser
-app.use(bodyParser.urlencoded({ extended: true }))
+// Apply css to localhost: 3000
+app.use(express.static("public"))
 
 // index.html
 app.get("/", function (req, res) {
