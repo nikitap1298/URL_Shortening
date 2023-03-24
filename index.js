97d086a37e8f0ca8d64a90b9e5a36807fbbf9ef9
Nikita <nikita.pishchugin@gmail.com>
2023-03-24 15:50:47 +0100

    Add ability to read data from form. Small changes

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
-// Apply css to localhost: 3000
+// Apply CSS to localhost: 3000
 app.use(express.static("public"))
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // index.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function (req, res) {
+  const userURL = String(req.body.userURL)
+
+  console.log("User URL is: " + userURL)
 })
 
 // Listener
