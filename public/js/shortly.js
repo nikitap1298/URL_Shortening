07bcf1e4cbf04547ed2e28019018fe453cbc92a3
Nikita <nikita.pishchugin@gmail.com>
2023-04-10 15:22:19 +0200

    Back-end and front-end are now working

--- public/js/shortly.js
+++ public/js/shortly.js
@@ -1,1 +1,36 @@
-const button = document.getElementById("shorten-it-button")
+const input = document.getElementById("input-form")
+const button = document.getElementById("button-form")
+
+function sendData(data) {
+  const XHR = new XMLHttpRequest()
+  const FD = new FormData()
+
+  // Push our data into our FormData object
+  for (const [name, value] of Object.entries(data)) {
+    FD.append(name, value)
+  }
+
+  // Define what happens on successful data submission
+  XHR.addEventListener("load", (event) => {
+    // alert('Yeah! Data sent and response loaded.');
+    if (input.value === "") {
+      input.placeholder = "Paste a link here!"
+      button.style.background = "red"
+    }
+  })
+
+  // Define what happens in case of an error
+  XHR.addEventListener("error", (event) => {
+    alert("Oops! Something went wrong.")
+  })
+
+  // Set up our request
+  XHR.open("POST", "http://localhost:3000/cors.php")
+
+  // Send our FormData object; HTTP headers are set automatically
+  XHR.send(FD)
+}
+
+button.addEventListener("click", () => {
+  sendData({ test: "ok" })
+})
